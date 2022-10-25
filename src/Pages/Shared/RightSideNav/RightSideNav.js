46687f7134ed5839ffc7ee253c50e1a59b5ad71b
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-25 23:45:18 +0600

    course name loaded from server

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const RightSideNav = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/all-courses')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    }, [])
     return (
         <div>
-            <h1>This is right side nav</h1>
+        {
+            courses.map(course => <p className='text-violet-400' key={course.id}>
+                <Link to={`/courses/${course.id}`}>{course.name}</Link>
+            </p>)
+        }
         </div>
     );
 };
