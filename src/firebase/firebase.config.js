a13125505a4f75d1bf2632936cf2650fef7e1da0
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-25 21:25:17 +0600

    mecEdu web application creating work started

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6B_7rYghyyzE6GhK7aUaulormdLOF7lc",
+  authDomain: "mecedu-f8fff.firebaseapp.com",
+  projectId: "mecedu-f8fff",
+  storageBucket: "mecedu-f8fff.appspot.com",
+  messagingSenderId: "403138194841",
+  appId: "1:403138194841:web:b2bc8b73e8813d25e87708"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
